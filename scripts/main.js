415b557c30faa59e11c137eec98c2a6c91165642
Sameer Mahant <3286086+sameermahant@users.noreply.github.com>
2020-09-04 22:29:40 +0530

    Changed avatar image format to WebP

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register("/service-worker.js")
-            .then(reg => { console.log("Service worker registered.") })
+            .then(reg => { console.log("Service worker registered. Scope is: ", reg.scope) })
             .catch(err => { console.log("Service worker registration failed.") });
     });
 } else {
